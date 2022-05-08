ecdc1f99d33ab9732d9e05538db383624d555826
Shahzad Gondal <102373581+shahzadahmed146@users.noreply.github.com>
2022-05-08 17:31:29 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
+    resource_group_name  = "Github-Actions-TF-demo"
+    storage_account_name = "tfstatefiledemo"
     container_name       = "tfstatefile"
     key                  = "dev.terraform.tfstate"
   }
